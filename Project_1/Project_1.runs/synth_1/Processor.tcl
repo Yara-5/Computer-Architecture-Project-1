ac129260e5c5ba031042c53fd44735d19b3200c7
Yara-5 <159156436+Yara-5@users.noreply.github.com>
2025-11-09 17:33:54 +0200

    Uploading Missing Files

--- Project_1/Project_1.runs/synth_1/Processor.tcl
+++ Project_1/Project_1.runs/synth_1/Processor.tcl
@@ -4,7 +4,7 @@
 
 set TIME_start [clock seconds] 
 namespace eval ::optrace {
-  variable script "C:/Users/Yara2005/Project 1/Computer-Architecture-Project-1/Project_1/Project_1.runs/synth_1/Processor.tcl"
+  variable script "C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.runs/synth_1/Processor.tcl"
   variable category "vivado_synth"
 }
 
@@ -76,32 +76,32 @@ create_project -in_memory -part xc7a100tcsg324-1
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir {C:/Users/Yara2005/Project 1/Computer-Architecture-Project-1/Project_1/Project_1.cache/wt} [current_project]
-set_property parent.project_path {C:/Users/Yara2005/Project 1/Computer-Architecture-Project-1/Project_1/Project_1.xpr} [current_project]
+set_property webtalk.parent_dir C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.cache/wt [current_project]
+set_property parent.project_path C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.xpr [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
 set_property board_part digilentinc.com:nexys-a7-100t:part0:1.2 [current_project]
-set_property ip_output_repo {c:/Users/Yara2005/Project 1/Computer-Architecture-Project-1/Project_1/Project_1.cache/ip} [current_project]
+set_property ip_output_repo c:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "Creating in-memory project" END { }
 OPTRACE "Adding files" START { }
 read_verilog -library xil_defaultlib {
-  {C:/Users/Yara2005/Project 1/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/ALU_Control.v}
-  {C:/Users/Yara2005/Project 1/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/Control_Unit.v}
-  {C:/Users/Yara2005/Project 1/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/DataMem.v}
-  {C:/Users/Yara2005/Project 1/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/FullAdder-1-1.v}
-  {C:/Users/Yara2005/Project 1/VerilogSrc/VerilogSrc/defines.v}
-  {C:/Users/Yara2005/Project 1/VerilogSrc/VerilogSrc/ImmGen.v}
-  {C:/Users/Yara2005/Project 1/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/InstMem.v}
-  {C:/Users/Yara2005/Project 1/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/RCA-1.v}
-  {C:/Users/Yara2005/Project 1/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/Register_File.v}
-  {C:/Users/Yara2005/Project 1/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/flipflop.v}
-  {C:/Users/Yara2005/Project 1/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/multiplexer.v}
-  {C:/Users/Yara2005/Project 1/VerilogSrc/VerilogSrc/prv32_ALU.v}
-  {C:/Users/Yara2005/Project 1/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/register.v}
-  {C:/Users/Yara2005/Project 1/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/shift_left.v}
-  {C:/Users/Yara2005/Project 1/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/shifter.v}
-  {C:/Users/Yara2005/Project 1/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/Processor.v}
+  C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/ALU_Control.v
+  C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/Control_Unit.v
+  C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/DataMem.v
+  C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/FullAdder-1-1.v
+  C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/defines.v
+  C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/ImmGen.v
+  C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/InstMem.v
+  C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/RCA-1.v
+  C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/Register_File.v
+  C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/flipflop.v
+  C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/multiplexer.v
+  C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/prv32_ALU.v
+  C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/register.v
+  C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/shift_left.v
+  C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/shifter.v
+  C:/Users/Yara2005/Computer-Architecture-Project-1/Project_1/Project_1.srcs/sources_1/new/Processor.v
 }
 OPTRACE "Adding files" END { }
 # Mark all dcp files as not used in implementation to prevent them from being
